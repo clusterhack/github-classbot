395494f8aaff50b9451ba3cf463e4e7d3c9e6b54
Spiros Papadimitriou <spapadim@gmail.com>
2026-03-17 11:54:33 -0400

    [fix] Use relative paths in knexfile
    No reason to do __dirname polyfills and other shennanigans. In fact,
    use of relative paths is strongly recommended (but template I'd found
    didn't, for some reason)

--- knexfile.ts
+++ knexfile.ts
@@ -1,10 +1,5 @@
 import type Knex from "knex";
 
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 const config: Knex.Config = {
   client: "mysql",
   connection: {
@@ -13,11 +8,11 @@ const config: Knex.Config = {
     password: process.env.CLASSBOT_DB_PASSWORD,
   },
   migrations: {
-    directory: __dirname + "/src/db/migrations",
+    directory: "./src/db/migrations",
     tableName: "knex_migrations",
   },
   seeds: {
-    directory: __dirname + "/src/db/seeds",
+    directory: "./src/db/seeds",
   },
 };
 
